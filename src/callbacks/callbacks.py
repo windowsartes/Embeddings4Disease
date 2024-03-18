6fb6030911343827142dc98c2d4d913c7187e099
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-03-18 18:38:00 +0300

    callbacks refactoring: now log directory creates inside associated callback

--- src/callbacks/callbacks.py
+++ src/callbacks/callbacks.py
@@ -3,26 +3,27 @@ import glob
 import pathlib
 import seaborn as sns
 from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
 import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import (
-    Trainer,
     TrainingArguments,
     TrainerCallback,
     TrainerState,
     TrainerControl,
 )
-from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 
 from data.datasets import CustomLineByLineDataset
 from data.collators import MaskingCollator
 from metrics.metrics import MetricComputer
+from utils import utils
 
 
-class MetricComputerValidationCallback(TrainerCallback):
+class MetricComputerCallback(TrainerCallback):
     """
-    This collator is used during training so we can evaluate model times after times.
+    This callback is used during training so we can evaluate model times after times.
     Using MaskingCollator it [MASK]s the last token in sequence and then asks model to fill the [MASK].
     Model will produce top_k best predictions and by their value will be validated using metrics you've
     specified in the 'use_metrics' argument. Please note that there can be only metrics  MetricComputer
@@ -33,7 +34,7 @@ class MetricComputerValidationCallback(TrainerCallback):
     Args:
         path_to_data (str): path to data on which you want to validate your model. It must by in line-by-line
             format: one transaction of icd10 codes on one line.
-        path_to_storage (str): path to directory where metrics values will be logged.
+        metrics_storage_dir (str): path to directory where metrics values will be logged.
         tokenizer (PreTrainedTokenizer | PreTrainedTokenizerFast): tokenizer which will be used to tokenize tha data inside dataloader.
         use_metrics (dict[str, bool]): indicated which metrics you want to use for validation.
         device (torch.device): by design we will validate you model using cpu so after the inference the model
@@ -46,8 +47,8 @@ class MetricComputerValidationCallback(TrainerCallback):
 
     def __init__(
         self,
-        path_to_data: str,
-        path_to_storage: str,
+        path_to_data: str | pathlib.Path,
+        metrics_storage_dir: str | pathlib.Path,
         tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
         use_metrics: dict[str, bool],
         device: torch.device,
@@ -58,7 +59,9 @@ class MetricComputerValidationCallback(TrainerCallback):
     ):
         super().__init__()
 
-        self.path_to_storage: str = path_to_storage
+        self.metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
+        utils.create_dir(self.metrics_storage_dir)
+
         self.period: int = period
 
         self.device: torch.device = device
@@ -70,11 +73,13 @@ class MetricComputerValidationCallback(TrainerCallback):
                 self.__initialize_metric_storage(metric)
 
         dataset: Dataset = CustomLineByLineDataset(path_to_data)
+
         dataloader: DataLoader = torch.utils.data.DataLoader(
             dataset,
-            batch_size=1024,
+            batch_size=batch_size,
             collate_fn=MaskingCollator(tokenizer, seq_len),
         )
+
         self.metric_computer: MetricComputer = MetricComputer(
             tokenizer, top_k, dataloader
         )
@@ -88,7 +93,7 @@ class MetricComputerValidationCallback(TrainerCallback):
         """
         self.__dump_logs(metric_name, {})
 
-    def __dump_logs(self, metric_name: str, logs: dict[int, float]) -> None:
+    def __dump_logs(self, metric_name: str, logs: dict[str, float]) -> None:
         """
         Dumps metric's loggs to its log file.
 
@@ -96,7 +101,7 @@ class MetricComputerValidationCallback(TrainerCallback):
             metric_name (str): metric's name.
             logs (dict[int, float]): metric's logs.
         """
-        file_name: pathlib.Path = pathlib.Path(self.path_to_storage).joinpath(
+        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
             f"{metric_name}.json"
         )
 
@@ -113,21 +118,20 @@ class MetricComputerValidationCallback(TrainerCallback):
         Returns:
             dict[str, float]: metric's logs.
         """
-        file_name: pathlib.Path = pathlib.Path(self.path_to_storage).joinpath(
+        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
             f"{metric_name}.json"
         )
 
         with open(file_name, "r") as file:
-            logs = json.load(file)
+            logs: dict[str, float] = json.load(file)
 
         return logs
 
-    def on_evaluate(
+    def on_evaluate(  # type: ignore
         self,
         args: TrainingArguments,
         state: TrainerState,
         control: TrainerControl,
-        model: PreTrainedModel,
         **kwargs,
     ) -> TrainerControl:
         """
@@ -138,19 +142,16 @@ class MetricComputerValidationCallback(TrainerCallback):
             args (TrainingArguments): Trainer's TrainingArguments; see docs for more details.
             state (TrainerState): Trainer's TrainerState; see docs for more details.
             control (TrainerControl): Trainer's TrainerControl; see docs for more details.
-            model (PreTrainedModel): model you train.
 
         Returns:
             TrainerControl: Trainer's TrainerControl.
         """
         if (state.epoch - 1) % self.period == 0:
-            control.should_save = False
-
             metrics: dict[str, float] = self.metric_computer.get_metrics_value(
-                model.to("cpu"), **self.use_metrics
+                kwargs["model"].to("cpu"), **self.use_metrics
             )
 
-            model.to(self.device)
+            kwargs["model"].to(self.device)
 
             for metric, usage in self.use_metrics.items():
                 if usage:
@@ -163,27 +164,59 @@ class MetricComputerValidationCallback(TrainerCallback):
 
 
 class SaveGraphsCallback(TrainerCallback):
-    def __init__(self, storage_path):
+    """
+    This callback is used to draw graphs with metrics' values and loss during training.
+
+    Args:
+        graph_storage_dir (str | pathlib.Path): path to directory where you want to save graphs.
+        metrics_storage_dir (str | pathlib.Path): path to directory where metrics' values are logged.
+    """
+
+    def __init__(
+        self,
+        graph_storage_dir: str | pathlib.Path,
+        metrics_storage_dir: str | pathlib.Path,
+    ):
         super().__init__()
-        self.storage_path = storage_path
+
+        self.graph_storage_dir: pathlib.Path = pathlib.Path(graph_storage_dir)
+        utils.create_dir(graph_storage_dir)
+
+        self.metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
+        utils.create_dir(metrics_storage_dir)
 
         sns.set_style("darkgrid")
         sns.set_palette("bright")
 
-    def on_train_end(self, args, state, control, **kwargs):
-        graph_storage_path = pathlib.Path(self.storage_path).joinpath("graph")
-        metric_storage_path = pathlib.Path(self.storage_path).joinpath("metrics")
+    def on_evaluate(  # type: ignore
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ) -> TrainerControl:
+        """
+        This method will be called on evaluation phase so we can compute and store metrics' values.
+        Loss history is stored in state argumnent so we'll use it from there.
 
-        metric_logs = glob.glob(str(metric_storage_path.joinpath("*.json")))
+        Args:
+            args (TrainingArguments): Trainer's TrainingArguments; see docs for more details.
+            state (TrainerState): Trainer's TrainerState; see docs for more details.
+            control (TrainerControl): Trainer's TrainerControl; see docs for more details.
+
+        Returns:
+            TrainerControl: Trainer's TrainerControl.
+        """
+
+        metric_logs: list[str] = glob.glob(
+            str(self.metrics_storage_dir.joinpath("*.json"))
+        )
 
         for metric_log in metric_logs:
             metric_name = pathlib.PurePath(metric_log).parts[-1][:-5]
 
-            with open(metric_logs, "r") as f:
-                metric_value = json.load(f)
-
             with open(pathlib.PurePath(metric_log), "r") as f:
-                metric_value = json.load(f)
+                metric_value: dict[str, float] = json.load(f)
 
             x_ticks: list[int] = [
                 int(float(value)) for value in list(metric_value.keys())
@@ -201,16 +234,14 @@ class SaveGraphsCallback(TrainerCallback):
             ax.grid(which="minor", alpha=0.2)
             ax.grid(which="major", alpha=0.5)
 
-            # plt.xticks(x_ticks, x_ticks)
             plt.xticks(rotation=45)
 
             ax.plot(x_ticks, values)
 
             plt.title(f"Значения {metric_name} на валидации")
             plt.xlabel("Эпоха")
-            plt.ylabel("Значени функции потерь")
 
-            plt.savefig(graph_storage_path.joinpath(f"{metric_name}.png"))
+            plt.savefig(self.graph_storage_dir.joinpath(f"{metric_name}.png"))
 
         history = state.log_history
 
@@ -236,27 +267,92 @@ class SaveGraphsCallback(TrainerCallback):
 
         plt.legend()
 
-        # plt.xticks(epoch_history, epoch_history)
         plt.xticks(rotation=45)
 
         plt.title("Изменение loss-функции за время обучения")
         plt.xlabel("Количество эпох")
-        plt.ylabel("Значение функции потерь")
 
         plt.legend()
 
-        plt.savefig(graph_storage_path.joinpath("loss.png"))
+        plt.savefig(self.graph_storage_dir.joinpath("loss.png"))
+
+        return control
 
 
 class SaveLossHistoryCallback(TrainerCallback):
-    def __init__(self, storage_path):
+    """
+    This callback is used to dump loss history to given file.
+
+    Args:
+        loss_storage_dir (str | pathlib.Path): directory where you want to store loss history.
+    """
+
+    def __init__(self, loss_storage_dir: str | pathlib.Path):
         super().__init__()
-        self.storage_path = storage_path
 
-    def on_train_end(self, args, state, control, **kwargs):
-        loss_path = pathlib.Path(self.storage_path).joinpath("loss")
+        self.loss_storage_dir: pathlib.Path = pathlib.Path(loss_storage_dir)
+        utils.create_dir(loss_storage_dir)
 
+    def on_evaluate(  # type: ignore
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ) -> TrainerControl:
+        """
+        This method will be called on evaluation phase so we can store loss history in the given file.
+
+        Args:
+            args (TrainingArguments): Trainer's TrainingArguments; see docs for more details.
+            state (TrainerState): Trainer's TrainerState; see docs for more details.
+            control (TrainerControl): Trainer's TrainerControl; see docs for more details.
+
+        Returns:
+            TrainerControl: Trainer's TrainerControl.
+        """
         history = state.log_history
 
-        with open(loss_path.joinpath("loss_history.json"), "w") as f:
+        with open(self.loss_storage_dir.joinpath("loss_history.json"), "w") as f:
             json.dump(history, f)
+
+        return control
+
+
+class FinalCheckpointCallback(TrainerCallback):
+    """
+    This callback is used to save trained model in the given file.
+
+    Args:
+        checkpoint_dir (str | pathlib.Path): directory where you want to save your model.
+    """
+
+    def __init__(self, checkpoint_dir: str | pathlib.Path):
+        super().__init__()
+
+        self.checkpoint_dir: pathlib.Path = pathlib.Path(checkpoint_dir)
+        utils.create_dir(checkpoint_dir)
+
+    def on_train_end(  # type: ignore
+        self,
+        args: TrainingArguments,
+        state: TrainerState,
+        control: TrainerControl,
+        **kwargs,
+    ) -> None:
+        """
+        This method will be called after the trainin so we can store save trained model.
+
+        Args:
+            args (TrainingArguments): Trainer's TrainingArguments; see docs for more details.
+            state (TrainerState): Trainer's TrainerState; see docs for more details.
+            control (TrainerControl): Trainer's TrainerControl; see docs for more details.
+        """
+        checkpoint = {
+            #'epoch': state.epoch,
+            "model": kwargs["model"],
+            #'optimizer': kwargs["optimizer"],
+            #'lr_scheduler': kwargs["lr_scheduler"],
+        }
+
+        torch.save(checkpoint, self.checkpoint_dir.joinpath("trained_model.pth"))
