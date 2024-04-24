d356f9a0ee88f1aea5dc4c2823b9882da81a24b9
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-24 23:15:59 +0300

    abstract factory for heads added

---
+++ src/embeddings4disease/head/factories/abstract_factory.py
@@ -0,0 +1,14 @@
+import os
+import typing as tp
+import yaml
+
+from embeddings4disease.head.factories import architecture_factory
+
+
+class AbstractFactory:
+    @staticmethod
+    def create(config_path: str) -> architecture_factory.ArchitectureFactory:
+        with open(os.path.abspath(config_path)) as f:
+            config: dict[str, tp.Any] = yaml.safe_load(f)
+
+        return architecture_factory.CLASS_REGISTER[config["model"]["type"]](config)
