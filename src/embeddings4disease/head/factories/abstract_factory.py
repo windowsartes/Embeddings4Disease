94b667cb1ff228854d8db92d382cf2f97c1deb5a
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-24 23:23:36 +0300

    lil naming update

--- src/embeddings4disease/head/factories/abstract_factory.py
+++ src/embeddings4disease/head/factories/abstract_factory.py
@@ -11,4 +11,4 @@ class AbstractFactory:
         with open(os.path.abspath(config_path)) as f:
             config: dict[str, tp.Any] = yaml.safe_load(f)
 
-        return architecture_factory.CLASS_REGISTER[config["model"]["type"]](config)
+        return architecture_factory.HEAD_REGISTER[config["model"]["type"]](config)
