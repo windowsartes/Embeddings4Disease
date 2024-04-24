72f7148f432e48c5dd3267bcc7438438f8abec22
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-24 23:02:32 +0300

    multilabel head architecture added

--- src/embeddings4disease/head/architectures/multilabel_head.py
+++ src/embeddings4disease/head/architectures/multilabel_head.py
@@ -1,6 +1,8 @@
+import warnings
 from collections import OrderedDict
 
 import torch
+import transformers
 from torch import nn
 from transformers import PreTrainedModel
 
@@ -23,8 +25,7 @@ class LinearBlock(nn.Module):
         ]))
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        print(input_tensor)
-        return self.block(input_tensor)
+        return self.block(input_tensor)  # type: ignore
 
 
 class MultiLabelHead(nn.Module):
@@ -32,27 +33,29 @@ class MultiLabelHead(nn.Module):
                  backbone: PreTrainedModel,
                  n_classes: int,
                  n_last_layers: int = 4,
-                 aggregation = "concatenation",
-                 mode = "transfer learning",
-                 hidden_sizes: tuple[int] = (1024, 512, 256, 128),
+                 aggregation: str = "concatenation",
+                 mode: str = "transfer learning",
+                 hidden_sizes: tuple[int, ...] = (1024, 512, 256,),
                  hidden_use_dropout: bool = True,
                  hidden_dropout_rate: float = 0.1,
-                 hidden_use_normalization: bool = True
+                 hidden_use_normalization: bool = True,
                 ):
         super().__init__()
 
         self.backbone: PreTrainedModel = backbone
         self.n_classes: int = n_classes
-        self.n_last_layers: int = min(n_last_layers, len(model.backbone.bert.encoder.layer))
+        self.n_last_layers: int = min(n_last_layers, len(self.backbone.bert.encoder.layer))
 
         if mode not in ["transfer learning", "fine tuning"]:
             warnings.warn(f"{mode} doen't supported; 'transfer learning' mode will be used",
                           UserWarning)
             self.mode = "transfer learning"
         else:
-            self.mode: str = mode
+            self.mode = mode
 
         if self.mode == "transfer learning":
+            for param in self.backbone.parameters():
+                param.requires_grad = False
             self.backbone.eval()
         elif self.mode == "fine tuning":
             self.backbone.train()
@@ -62,21 +65,27 @@ class MultiLabelHead(nn.Module):
                           UserWarning)
             self.aggregation = "concatenation"
         else:
-            self.aggregation: str = aggregation
+            self.aggregation = aggregation
 
         embedding_size: int = self.backbone.bert.embeddings.word_embeddings.embedding_dim
 
-        if aggregation == "concatenation":
+        if self.aggregation == "concatenation":
             input_size = embedding_size * self.n_last_layers
-        elif aggregation == "addition":
+        elif self.aggregation == "addition":
             input_size = embedding_size
 
         self.input_block: LinearBlock = LinearBlock(input_size, hidden_sizes[0])
 
         self.hidden_blocks: nn.Sequential = nn.Sequential(OrderedDict([
             *[(f"hidden_block_{i}",
-               LinearBlock(hidden_sizes[i], hidden_sizes[i+1], use_dropout,
-                           dropout_rate, use_normalization))
+               LinearBlock(
+                           hidden_sizes[i],
+                           hidden_sizes[i+1],
+                           hidden_use_dropout,
+                           hidden_dropout_rate,
+                           hidden_use_normalization
+                          )
+              )
              for i in range(len(hidden_sizes) - 1)]
         ]))
 
@@ -100,5 +109,4 @@ class MultiLabelHead(nn.Module):
                                      dim=-1
                                     ).sum(dim=-1)[:, 0, :]
 
-        return self.classification_head(self.hidden_blocks(self.input_block(embeddings)))
-    
+        return self.classification_head(self.hidden_blocks(self.input_block(embeddings)))  # type: ignore
