f2a77e496185af43cba38bf41c19e6a884fc1616
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-26 11:58:01 +0300

    lil naming update for head's abstract factory

--- src/embeddings4disease/head/factories/abstract_factory.py
+++ src/embeddings4disease/head/factories/abstract_factory.py
@@ -2,13 +2,13 @@ import os
 import typing as tp
 import yaml
 
-from embeddings4disease.head.factories import architecture_factory
+from embeddings4disease.head.factories import head_factory
 
 
 class AbstractFactory:
     @staticmethod
-    def create(config_path: str) -> architecture_factory.HeadFactory:
+    def create(config_path: str) -> head_factory.HeadFactory:
         with open(os.path.abspath(config_path)) as f:
             config: dict[str, tp.Any] = yaml.safe_load(f)
 
-        return architecture_factory.HEAD_REGISTER[config["model"]["type"]](config)
+        return head_factory.HEAD_REGISTER[config["model"]["type"]](config)
