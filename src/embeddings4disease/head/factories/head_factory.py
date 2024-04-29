3209fb9e9cb58c6a5b16435abb558d7e0f33ce62
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-29 23:02:47 +0300

    head factory interface and multilabel head factory added

--- src/embeddings4disease/head/factories/head_factory.py
+++ src/embeddings4disease/head/factories/head_factory.py
@@ -1,21 +1,29 @@
 import os
+import pathlib
 import typing as tp
 from abc import ABC, abstractmethod
 
 import torch
+from datetime import datetime
 from torch.utils.data import DataLoader
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 
+from embeddings4disease.callbacks import custom_callbacks
 from embeddings4disease.head.architectures import multilabel_head
 from embeddings4disease.data.datasets import MultiLabelHeadDataset
 from embeddings4disease.data.collators import MultiLabelHeadCollator
 from embeddings4disease.trainer import TrainingArgs
+from embeddings4disease.utils import utils
 
 
 class HeadFactory(ABC):
     def __init__(self, config: dict[str, tp.Any]):
         self.config: dict[str, tp.Any] = config
+
+    @abstractmethod
+    def initialize(self) -> None:
+        pass
 
     @abstractmethod
     def create_model(self) -> torch.nn.Module:
@@ -30,7 +38,7 @@ class HeadFactory(ABC):
         pass
 
     @abstractmethod
-    def create_callbacks(self) -> list[tp.Any]:  # just for now
+    def create_callbacks(self) -> list[custom_callbacks.CustomCallback]:
         pass
 
     @abstractmethod
@@ -50,6 +58,9 @@ def head(cls: tp.Type[HeadFactory]) -> tp.Type[HeadFactory]:
 class MultiLabelHeadFactory(HeadFactory):
     def __init__(self, config: dict[str, tp.Any]):
         super().__init__(config)
+
+    def initialize(self) -> None:
+        self._create_storage()
 
     def create_model(self) -> multilabel_head.MultiLabelHead:
         tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast = self.load_tokenizer()
@@ -98,8 +109,40 @@ class MultiLabelHeadFactory(HeadFactory):
             drop_last=True,
         )
 
-    def create_callbacks(self) -> list[tp.Any]:
-        return []
+    def create_callbacks(self) -> list[custom_callbacks.CustomCallback]:
+        used_callbacks: list[custom_callbacks.CustomCallback] = []
+
+        compute_metrics: bool = False
+        for value in self.config["validation"]["metrics"].values():
+            if value:
+                compute_metrics = value
+                break
+
+        if compute_metrics:
+            device: torch.device = torch.device(
+                self.config["training"]["device"]
+                if torch.cuda.is_available()
+                else "cpu"
+            )
+
+            used_callbacks.append(
+                custom_callbacks.MetricComputerCallback(
+                    metrics_storage_dir=self.storage_path.joinpath("metrics"),
+                    use_metrics=self.config["validation"]["metrics"],
+                    dataloader=self.create_dataloader("validation"),
+                    device=device,
+                    period=self.config["validation"]["period"],
+                    threshold=self.config["validation"]["threshold"],
+                    save_plot=True,
+                )
+            )
+
+        used_callbacks.append(custom_callbacks.SaveLossHistoryCallback(self.storage_path.joinpath("loss"), True))
+
+        used_callbacks.append(custom_callbacks.CheckpointCallback(self.storage_path.joinpath("checkpoint")))
+        used_callbacks.append(custom_callbacks.SaveBestModelCallback(self.storage_path.joinpath("best_model")))
+
+        return used_callbacks
 
     def create_training_args(self) -> TrainingArgs:
         return TrainingArgs(
@@ -110,3 +153,17 @@ class MultiLabelHeadFactory(HeadFactory):
             criterion=torch.nn.BCEWithLogitsLoss,
             **self.config["training"]["optimizer_parameters"],
         )
+
+    def _create_storage(self) -> None:
+        """
+        This method is used to initialize storage dir in the case you need to store logs/graphs/etc somewhere.
+        """
+        working_dir: pathlib.Path = pathlib.Path(utils.get_cwd())
+
+        now = datetime.now()
+        data, time = now.strftime("%b-%d-%Y %H:%M").replace(":", "-").split()
+
+        storage_path = working_dir.joinpath(self.config["model"]["type"]).joinpath(data).joinpath(time)
+        utils.create_dir(storage_path)
+
+        self.storage_path: pathlib.Path = storage_path
