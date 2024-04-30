f0054ef58202b8d91441df36eb0a819aa3e2697a
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-04-30 14:37:31 +0300

    cli for mlm preprocessing updated

--- src/embeddings4disease/cli/preprocess_mlm.py
+++ src/embeddings4disease/cli/preprocess_mlm.py
@@ -7,7 +7,7 @@ from embeddings4disease.preprocessing import abstract_factory
 @click.argument("config_path", type=click.Path(exists=True))
 def preprocess(config_path: str) -> None:
     factory = abstract_factory.AbstractFactory().create(config_path)
-    factory.make_train_val_split()
+    factory.create_mlm_dataset()
     factory.create_vocab()
 
 
