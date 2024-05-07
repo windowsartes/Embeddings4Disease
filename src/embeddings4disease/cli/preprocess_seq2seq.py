e64fd0a0d43a1b21a6f5f641cd22de7ce1c63c71
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-05-07 14:20:51 +0300

    cli for seq2seq dataset creation added

---
+++ src/embeddings4disease/cli/preprocess_seq2seq.py
@@ -0,0 +1,14 @@
+import click
+
+from embeddings4disease.preprocessing import abstract_factory
+
+
+@click.command()
+@click.argument("config_path", type=click.Path(exists=True))
+def preprocess(config_path: str) -> None:
+    factory = abstract_factory.AbstractFactory().create(config_path)
+    factory.create_seq2seq_dataset()
+
+
+if __name__ == "__main__":
+    preprocess()
