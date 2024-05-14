6d0f632309e4fd9dd8cfd2b1c27182fccfceed09
Nikolai Makarov <blackcatsindikete@gmail.com>
2024-05-14 11:22:03 +0300

    Revert "Nsp"

---
+++ src/cli/validation.py
@@ -0,0 +1,18 @@
+import click
+
+from factories import abstract_factory
+
+
+@click.command()
+@click.argument("config_path", type=click.Path(exists=True))
+def validation(config_path: str) -> None:
+    factory = abstract_factory.AbstractFactory().create(config_path)
+    model = factory.create_model()
+
+    metric_computer, used_metrics = factory.create_metric_computer()
+
+    print(metric_computer.get_metrics_value(model, **used_metrics))
+
+
+if __name__ == "__main__":
+    validation()
---
+++ src/data/preprocessing/abstract_factory.py
@@ -0,0 +1,18 @@
+import os
+import pathlib
+import typing as tp
+import yaml
+
+from data.preprocessing import preprocessor_factory
+
+
+working_dir: pathlib.Path = pathlib.Path(os.getcwd())
+
+
+class AbstractFactory:
+    @staticmethod
+    def create(config_path: str) -> preprocessor_factory.PreprocessorFactory:
+        with open(working_dir.joinpath(config_path)) as f:
+            config: dict[str, tp.Any] = yaml.safe_load(f)
+
+        return preprocessor_factory.CLASS_REGISTER[config["dataset"]](config)
