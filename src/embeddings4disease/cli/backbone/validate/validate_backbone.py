c42aecf8c64a587cef06b637a9d9f75a601e1771
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-05-24 19:31:38 +0300

    config examples and cli for backbone validation updated

---
+++ src/embeddings4disease/cli/backbone/validate/validate_backbone.py
@@ -0,0 +1,18 @@
+import click
+
+from embeddings4disease.backbone.factories import abstract_factory
+
+
+@click.command()
+@click.argument("config_path", type=click.Path(exists=True))
+def validate(config_path: str) -> None:
+    factory = abstract_factory.AbstractFactory().create(config_path)
+    model = factory.create_model()
+
+    metric_computer = factory.create_metric_computer()
+
+    print(metric_computer.get_metrics_value(model.eval()))
+
+
+if __name__ == "__main__":
+    validate()
