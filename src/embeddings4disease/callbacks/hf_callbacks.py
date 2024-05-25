e3e0f73a6b7c32ceb9473b5ebebdd672d8339966
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-05-25 23:10:32 +0300

    callback refactored

--- src/embeddings4disease/callbacks/hf_callbacks.py
+++ src/embeddings4disease/callbacks/hf_callbacks.py
@@ -22,10 +22,9 @@ except ImportError:
 else:
     wandb_installed = True
 
-from embeddings4disease.data.datasets import SourceTargetStringsDataset, CustomLineByLineDataset
+from embeddings4disease.data.datasets import CustomLineByLineDataset
 from embeddings4disease.data.collators import MaskingCollator
 from embeddings4disease.metrics.backbone_metrics import MLMMetricComputer
-from embeddings4disease.metrics.multilabel_head_metrics import EncoderDecoderHeadMetricComputer
 from embeddings4disease.utils import utils
 
 
@@ -71,22 +70,22 @@ class MetricComputerCallback(TrainerCallback):
     ):
         super().__init__()
 
-        self.metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
-        utils.create_dir(self.metrics_storage_dir)
+        self._metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
+        utils.create_dir(self._metrics_storage_dir)
 
-        self.period: int = period
+        self._period: int = period
 
-        self.device: torch.device = device
+        self._device: torch.device = device
 
-        self.use_metrics: dict[str, bool] = use_metrics
-        self.use_wandb: bool = use_wandb
-        self.save_plot: bool = save_plot
+        self._use_metrics: dict[str, bool] = use_metrics
+        self._use_wandb: bool = use_wandb
+        self._save_plot: bool = save_plot
 
-        if self.save_plot:
+        if self._save_plot:
             sns.set_style("white")
             sns.set_palette("bright")
 
-        for metric, usage in self.use_metrics.items():
+        for metric, usage in self._use_metrics.items():
             if usage:
                 self.__initialize_metric_storage(metric)
 
@@ -98,8 +97,8 @@ class MetricComputerCallback(TrainerCallback):
             collate_fn=MaskingCollator(tokenizer, seq_len),
         )
 
-        self.metric_computer: MLMMetricComputer = MLMMetricComputer(
-            tokenizer, top_k, dataloader
+        self._metric_computer: MLMMetricComputer = MLMMetricComputer(
+            tokenizer, top_k, dataloader, self._use_metrics
         )
 
     def __initialize_metric_storage(self, metric_name: str) -> None:
@@ -119,10 +118,10 @@ class MetricComputerCallback(TrainerCallback):
             metric_name (str): metric's name.
             logs (dict[int, float]): metric's logs.
         """
-        utils.create_dir(pathlib.Path(self.metrics_storage_dir).joinpath(
+        utils.create_dir(pathlib.Path(self._metrics_storage_dir).joinpath(
             pathlib.Path(metric_name)))
 
-        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
+        file_name: pathlib.Path = pathlib.Path(self._metrics_storage_dir).joinpath(
             pathlib.Path(metric_name).joinpath(f"{metric_name}.json")
         )
 
@@ -139,7 +138,7 @@ class MetricComputerCallback(TrainerCallback):
         Returns:
             dict[str, float]: metric's logs.
         """
-        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
+        file_name: pathlib.Path = pathlib.Path(self._metrics_storage_dir).joinpath(
             pathlib.Path(metric_name).joinpath(f"{metric_name}.json")
         )
 
@@ -167,21 +166,19 @@ class MetricComputerCallback(TrainerCallback):
         Returns:
             TrainerControl: Trainer's TrainerControl.
         """
-        if (state.epoch - 1) % self.period == 0:
-            metrics: dict[str, float] = self.metric_computer.get_metrics_value(  # type: ignore[assignment]
-                kwargs["model"].to("cpu"), self.use_metrics
-            )
+        if (state.epoch - 1) % self._period == 0:
+            metrics: dict[str, float] = self._metric_computer.get_metrics_value(kwargs["model"].to("cpu"))  # type: ignore[assignment]
 
-            kwargs["model"].to(self.device)
+            kwargs["model"].to(self._device)
 
-            for metric_name, usage in self.use_metrics.items():
+            for metric_name, usage in self._use_metrics.items():
                 if usage:
                     logs = self.__load_logs(metric_name)
                     logs[state.epoch] = metrics[metric_name]
 
                     self.__dump_logs(metric_name, logs)
 
-                    if self.save_plot:
+                    if self._save_plot:
                         sns.set()
 
                         x_ticks: list[int] = [
@@ -207,14 +204,18 @@ class MetricComputerCallback(TrainerCallback):
                         plt.title(f"Value of {metric_name} on validation")
                         plt.xlabel("Epoch")
 
-                        plt.savefig(self.metrics_storage_dir.joinpath(
+                        plt.savefig(self._metrics_storage_dir.joinpath(
                             pathlib.Path(metric_name).joinpath(f"{metric_name}.png")
                         ))
 
                         plt.close(fig)
 
-            if wandb_installed and self.use_wandb:
-                wandb.log({f"eval/{metric_name}": metrics[metric_name] for metric_name, usage in self.use_metrics.items() if usage})
+            if wandb_installed and self._use_wandb:
+                wandb.log(
+                    {
+                        f"eval/{metric_name}": metrics[metric_name] for metric_name, usage in self._use_metrics.items() if usage
+                    }
+                )
 
         return control
 
@@ -229,12 +230,12 @@ class SaveLossHistoryCallback(TrainerCallback):
     def __init__(self, loss_storage_dir: str | pathlib.Path, save_plot: bool):
         super().__init__()
 
-        self.loss_storage_dir: pathlib.Path = pathlib.Path(loss_storage_dir)
+        self._loss_storage_dir: pathlib.Path = pathlib.Path(loss_storage_dir)
         utils.create_dir(loss_storage_dir)
 
-        self.save_plot: bool = save_plot
+        self._save_plot: bool = save_plot
 
-        if self.save_plot:
+        if self._save_plot:
             sns.set_style("white")
             sns.set_palette("bright")
 
@@ -258,10 +259,10 @@ class SaveLossHistoryCallback(TrainerCallback):
         """
         history = state.log_history
 
-        with open(self.loss_storage_dir.joinpath("loss_history.json"), "w") as f:
+        with open(self._loss_storage_dir.joinpath("loss_history.json"), "w") as f:
             json.dump(history, f)
 
-        if self.save_plot:
+        if self._save_plot:
             sns.set()
 
             loss_train_history = [record["loss"] for record in history if "loss" in record]
@@ -281,8 +282,8 @@ class SaveLossHistoryCallback(TrainerCallback):
             ax.grid(which="minor", alpha=0.2)
             ax.grid(which="major", alpha=0.5)
 
-            ax.plot(epoch_history, loss_train_history, label="Training")
-            ax.plot(epoch_history, loss_val_history, label="Validation")
+            ax.plot(epoch_history, loss_train_history, label="Training", color="red")
+            ax.plot(epoch_history, loss_val_history, label="Validation", color="blue")
 
             plt.xticks(rotation=45)
 
@@ -290,106 +291,38 @@ class SaveLossHistoryCallback(TrainerCallback):
             plt.xlabel("Number of epochs")
             plt.legend()
 
-            plt.savefig(self.loss_storage_dir.joinpath("loss.png"))
+            plt.savefig(self._loss_storage_dir.joinpath("loss.png"))
 
             plt.close(fig)
 
         return control
 
-class EncoderDecoderMetricComputerCallback(TrainerCallback):
+
+class EncoderDecoderMetricPlotsCallback(TrainerCallback):
     def __init__(
         self,
-        path_to_data: str | pathlib.Path,
         metrics_storage_dir: str | pathlib.Path,
-        tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
         use_metrics: dict[str, bool],
-        device: torch.device,
-        period: int = 10,
-        batch_size: int = 1024,
-        seq_len: int = 24,
         use_wandb: bool = False,
         save_plot: bool = True,
     ):
         super().__init__()
 
-        self.metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
-        utils.create_dir(self.metrics_storage_dir)
+        self._metrics_storage_dir: pathlib.Path = pathlib.Path(metrics_storage_dir)
+        utils.create_dir(self._metrics_storage_dir)
 
-        self.period: int = period
+        self._use_metrics: dict[str, bool] = use_metrics
 
-        self.device: torch.device = device
+        for metric, usage in self._use_metrics.items():
+            if usage:
+                pathlib.Path(self._metrics_storage_dir).joinpath(pathlib.Path(metric)).mkdir(parents=True, exist_ok=True)
 
-        self.use_metrics: dict[str, bool] = use_metrics
-        self.use_wandb: bool = use_wandb
-        self.save_plot: bool = save_plot
+        self._use_wandb: bool = use_wandb
+        self._save_plot: bool = save_plot
 
-        if self.save_plot:
+        if self._save_plot:
             sns.set_style("white")
             sns.set_palette("bright")
-
-        for metric, usage in self.use_metrics.items():
-            if usage:
-                self.__initialize_metric_storage(metric)
-
-        dataset: Dataset = SourceTargetStringsDataset(path_to_data)
-
-        dataloader: DataLoader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch_size,
-        )
-
-        self.metric_computer: EncoderDecoderHeadMetricComputer = EncoderDecoderHeadMetricComputer(
-            dataloader,
-            device,
-            tokenizer,
-            seq_len,
-        )
-
-    def __initialize_metric_storage(self, metric_name: str) -> None:
-        """
-        For every metric there is its own file; this method initialize it with empty object.
-
-        Args:
-            metric_name (str): metric's name; it will be used as a filename.
-        """
-        self.__dump_logs(metric_name, {})
-
-    def __dump_logs(self, metric_name: str, logs: dict[str, float]) -> None:
-        """
-        Dumps metric's loggs to its log file.
-
-        Args:
-            metric_name (str): metric's name.
-            logs (dict[int, float]): metric's logs.
-        """
-        utils.create_dir(pathlib.Path(self.metrics_storage_dir).joinpath(
-            pathlib.Path(metric_name)))
-
-        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
-            pathlib.Path(metric_name).joinpath(f"{metric_name}.json")
-        )
-
-        with open(file_name, "w") as file:
-            json.dump(logs, file)
-
-    def __load_logs(self, metric_name: str) -> dict[str, float]:
-        """
-        Loads metric's logs from its log file.
-
-        Args:
-            metric_name (str):  metric's name.
-
-        Returns:
-            dict[str, float]: metric's logs.
-        """
-        file_name: pathlib.Path = pathlib.Path(self.metrics_storage_dir).joinpath(
-            pathlib.Path(metric_name).joinpath(f"{metric_name}.json")
-        )
-
-        with open(file_name, "r") as file:
-            logs: dict[str, float] = json.load(file)
-
-        return logs
 
     def on_evaluate(  # type: ignore
         self,
@@ -398,66 +331,50 @@ class EncoderDecoderMetricComputerCallback(TrainerCallback):
         control: TrainerControl,
         **kwargs,
     ) -> TrainerControl:
-        """
-        This method will be called by Trainer on evaluation phase.
-        Using MetricComputer it will compute metric you've selected in use_metrics argument and log them.
+        history = state.log_history
 
-        Args:
-            args (TrainingArguments): Trainer's TrainingArguments; see docs for more details.
-            state (TrainerState): Trainer's TrainerState; see docs for more details.
-            control (TrainerControl): Trainer's TrainerControl; see docs for more details.
+        for metric, usage in self._use_metrics.items():
+            if usage:
+                metric_value: dict[int, float] = {}
 
-        Returns:
-            TrainerControl: Trainer's TrainerControl.
-        """
-        if (state.epoch - 1) % self.period == 0:
-            metrics: dict[str, float] = self.metric_computer.get_metrics_value(  # type: ignore[assignment]
-                kwargs["model"].to("cpu"),
-                self.use_metrics
-            )
+                for step in history:
+                    if f"eval_{metric}" in step:
+                        metric_value[int(step["epoch"])] = step[f"eval_{metric}"]
 
-            kwargs["model"].to(self.device)
+                file_name: pathlib.Path = pathlib.Path(self._metrics_storage_dir).joinpath(
+                    pathlib.Path(metric).joinpath(f"{metric}.json")
+                )
 
-            for metric_name, usage in self.use_metrics.items():
-                if usage:
-                    logs = self.__load_logs(metric_name)
-                    logs[state.epoch] = metrics[metric_name]
+                with open(file_name, "w") as file_json:
+                    json.dump(metric_value, file_json)
 
-                    self.__dump_logs(metric_name, logs)
+                if self._save_plot:
+                    sns.set()
 
-                    if self.save_plot:
-                        sns.set()
+                    fig = plt.figure()
+                    ax = fig.add_subplot(1, 1, 1)
 
-                        x_ticks: list[int] = [
-                            int(float(value)) for value in list(logs.keys())
-                        ]
-                        values: list[float] = list(logs.values())
+                    ax.yaxis.set_minor_locator(AutoMinorLocator())
 
-                        fig = plt.figure()
-                        ax = fig.add_subplot(1, 1, 1)
+                    plt.plot(list(metric_value.keys()), list(metric_value.values()), color="blue")
 
-                        ax.yaxis.set_minor_locator(AutoMinorLocator())
+                    ax.tick_params(which="major", length=7)
+                    ax.tick_params(which="minor", length=4, color="red")
 
-                        ax.tick_params(which="major", length=7)
-                        ax.tick_params(which="minor", length=4, color="r")
+                    ax.grid(which="minor", alpha=0.2)
+                    ax.grid(which="major", alpha=0.5)
 
-                        ax.grid(which="minor", alpha=0.2)
-                        ax.grid(which="major", alpha=0.5)
+                    plt.xticks(rotation=45)
 
-                        plt.xticks(rotation=45)
+                    plt.title(f"Value of {metric} on validation\n during training")
+                    plt.xlabel("Number of epochs")
 
-                        ax.plot(x_ticks, values)
+                    file_name_img: pathlib.Path = pathlib.Path(self._metrics_storage_dir).joinpath(
+                        pathlib.Path(metric).joinpath(f"{metric}.png")
+                    )
 
-                        plt.title(f"Value of {metric_name} on validation")
-                        plt.xlabel("Epoch")
+                    plt.savefig(file_name_img)
 
-                        plt.savefig(self.metrics_storage_dir.joinpath(
-                            pathlib.Path(metric_name).joinpath(f"{metric_name}.png")
-                        ))
-
-                        plt.close(fig)
-
-            if wandb_installed and self.use_wandb:
-                wandb.log({f"eval/{metric_name}": metrics[metric_name] for metric_name, usage in self.use_metrics.items() if usage})
+                    plt.close(fig)
 
         return control
